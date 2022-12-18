5213f825a48346128a849e2e61de718edcc41f9c
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-12-18 08:17:09 +0530

    Data Types

---
+++ 01_JavaScriptEssentialTraining/19_DataTypes/script.js
@@ -0,0 +1,40 @@
+/*
+Data Types in JAvaScript
+*/
+
+// string
+let stringDemo = "A string of text";
+console.log("string:",stringDemo);
+
+// Number:
+let integerDemo = 4;
+console.log("Integer:",integerDemo);
+
+let floatDemo = 5.6;
+console.log("float:",floatDemo);
+
+// Boolean
+let boolean = true;
+console.log("Boolean value:",boolean);
+
+// null value
+let nullDemo = null;
+console.log("Null value:",nullDemo);
+
+// Undefined
+let undefinedDemo;
+console.log("Undefined:",undefinedDemo);
+
+let UndefinedAssignedDemo = undefined;
+console.log("Undefined assigned:", UndefinedAssignedDemo);
+
+// Objects
+const objectDemo = {
+    dance: "Mambo",
+    number: 5,
+};
+console.log("Object:",objectDemo);
+
+// Array
+const arrayDemo = ['a','b','c','abc'];
+console.log("Array:",arrayDemo)
