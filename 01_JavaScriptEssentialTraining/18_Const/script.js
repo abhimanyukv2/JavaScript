098d173de4a24ff8f95a97deaabd722cfd9e9994
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-12-18 08:04:55 +0530

    Scope and Const

---
+++ 01_JavaScriptEssentialTraining/18_Const/script.js
@@ -0,0 +1,25 @@
+/*
+Scope and the variable statement
+*/
+
+const color = "purple";
+
+document.querySelector(".left").style.backgroundColor = color;
+document.querySelector(".left .color-value").innerHTML = color;
+
+color = "skyblue";
+
+
+function headingColor() {
+    let titleColor = "blue";
+    document.querySelector(".title").style.color = titleColor;
+    console.log('inside:',titleColor)
+}
+
+
+headingColor()
+
+document.querySelector(".right").style.backgroundColor = color;
+document.querySelector(".right .color-value").innerHTML = color;
+
+// console.log('outside:',titleColor)
