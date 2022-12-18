8571513c0d6151552aa44c625074c2ae1fef63bc
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-12-18 08:45:31 +0530

    array in codes

---
+++ 01_JavaScriptEssentialTraining/20_Array/script.js
@@ -0,0 +1,14 @@
+let item = "flashlight";
+const collection = ['Piggy', item, 5, true]
+console.log(collection)
+
+console.log(collection.length)
+
+console.log(collection[1])
+collection[2] = "camera";
+console.log(collection[2])
+collection[collection.length] = "new item"
+console.log(collection)
+collection[9] = "at the end"
+console.log(collection)
+console.log(collection[8])
