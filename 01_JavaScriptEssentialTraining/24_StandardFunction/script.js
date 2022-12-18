17b1f06b249e23eb720066ee904153a3a2ff8078
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-12-18 14:15:11 +0530

    A standaer function

---
+++ 01_JavaScriptEssentialTraining/24_StandardFunction/script.js
@@ -0,0 +1,27 @@
+// Standaed Function
+
+const greenPack = {
+    name: "Frog Pack",
+    color: "green",
+    volume: 8,
+    pocketNum: 3,
+};
+
+const addPack = function(currentPack) {
+    const newArticle = document.createElement("article");
+    newArticle.innerHTML = `
+        <h1>${currentPack.name}</h1>
+        <ul>
+            <li>Volume: ${currentPack.color}</li>
+            <li>Color: ${currentPack.volume}</li>
+            <li>Number of pockets: ${currentPack.pocketNum}</li>
+        </ul>
+    `;
+    return newArticle;
+};
+
+const theArticle = addPack(greenPack);
+console.log(theArticle);
+
+const main = document.querySelector("main");
+main.append(addPack(greenPack));
