b6739bdeb59b92db87787dc4b721ac12b2edbdec
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-12-19 08:31:03 +0530

    Pass Data to a Function with Parameter

---
+++ 01_JavaScriptEssentialTraining/26_PassDataToFunction/script.js
@@ -0,0 +1,27 @@
+// Pass data to function through parameter
+
+const tipCalculator = (sum, percentage, currency, prefix) => {
+    // let sum = 29.95;
+    // let percentage = 18;
+    let tip = sum*(percentage/100);
+    let total = sum + tip;
+    if (prefix) {
+        console.log(`
+            Sum before tip: ${currency}${sum}
+            Tip percentage: ${percentage}%
+            Tip:            ${currency}${tip.toFixed(2)}
+            Total:          ${currency}${total.toFixed(2)}
+        `);
+        
+    }
+    else {
+        console.log(`
+            Sum before tip: ${sum}${currency}
+            Tip percentage: ${percentage}%
+            Tip:            ${tip.toFixed(2)}${currency}
+            Total:          ${total.toFixed(2)}${currency}
+        `);
+    };
+};
+
+tipCalculator(29.95, 28, '$', true);
